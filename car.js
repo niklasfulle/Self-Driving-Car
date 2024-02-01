4896571f292a8601d0140fb62801514198e42f5a
niklasfulle <fulle.niklas@web.de>
2024-02-01 15:41:31 +0100

    added sensors and collision detection

--- car.js
+++ car.js
@@ -10,12 +10,63 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.damage = false;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
-    this.#move();
+  update(roadBorders) {
+    if (!this.damage) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damage = this.#assessDamage(roadBorders);
+    }
+    this.sensor.update(roadBorders);
+  }
+
+  #assessDamage(roadBorders) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * createPolygon - Creates the polygon of the car
+   * @returns {Array} Array of points
+   */
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    // top right point
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+
+    // top left point
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+
+    // bottom left point
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+
+    // bottom right point
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+    return points;
   }
 
   #move() {
@@ -39,35 +90,37 @@ class Car {
     if (this.speed < 0) {
       this.speed += this.friction;
     }
-
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
 
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
-      if (this.controls.right) {
-        this.angle -= 0.03 * flip;
-      }
-
       if (this.controls.left) {
         this.angle += 0.03 * flip;
       }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
 
-    this.x -= this.speed * Math.sin(this.angle);
-    this.y -= this.speed * Math.cos(this.angle);
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-
+    if (this.damage) {
+      ctx.fillStyle = "red";
+    } else {
+      ctx.fillStyle = "black";
+    }
     ctx.beginPath();
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
     ctx.fill();
 
-    ctx.restore();
+    this.sensor.draw(ctx);
   }
 }
--- controls.js
+++ controls.js
@@ -1,9 +1,9 @@
 class Controls {
   constructor() {
     this.forward = false;
+    this.left = false;
     this.right = false;
     this.reverse = false;
-    this.left = false;
 
     this.#addKeyboardListeners();
   }
@@ -11,34 +11,33 @@ class Controls {
   #addKeyboardListeners() {
     document.onkeydown = (event) => {
       switch (event.key) {
-        case "ArrowUp":
-          this.forward = true;
+        case "ArrowLeft":
+          this.left = true;
           break;
         case "ArrowRight":
           this.right = true;
           break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
         case "ArrowDown":
           this.reverse = true;
           break;
-        case "ArrowLeft":
-          this.left = true;
-          break;
       }
     };
-
     document.onkeyup = (event) => {
       switch (event.key) {
-        case "ArrowUp":
-          this.forward = false;
+        case "ArrowLeft":
+          this.left = false;
           break;
         case "ArrowRight":
           this.right = false;
           break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
         case "ArrowDown":
           this.reverse = false;
-          break;
-        case "ArrowLeft":
-          this.left = false;
           break;
       }
     };
